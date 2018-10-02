e120fe3fc3472be0fd319593d463040f3b212022
aryrachmadis <43523730+aryrachmadis@users.noreply.github.com>
2018-10-02 16:51:24 +0700

    Update regristrasi.php

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
-
-
-
-</body>
-</html>
+<form action="koneksiregis.php" method="POST">
+	<table>
+		<tr>
+			<td><b>Pendaftaran Mahasiswa</b></td>
+		</tr>
+		<tr>
+			<td>Nim</td>
+			<td>:</td>
+			<td><input type="text" name="nim" minlength="10" maxlength="10"></td>
+		</tr>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td>:</td>
+			<td><input type="email" name="email" value="@gmail.com"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="submit" value="Daftar"></td>
+		</tr>
+		
+	</table>
+</form>
