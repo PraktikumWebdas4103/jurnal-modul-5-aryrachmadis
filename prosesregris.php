45d49dd7835d1a874befc8f2c118cc258bd61287
aryrachmadis <43523730+aryrachmadis@users.noreply.github.com>
2018-10-02 16:34:51 +0700

    Update prosesregris.php

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,24 @@
 <?php
+$host = "localhost"; //host server
+$user = "root"; //user login phpmyadmin 
+$inpt = ""; //user login phpmyadmin
+$db = "jurnal"; //nama database
+
+if (isset($_POST['submit'])) {
+			$nim = $_REQUEST['nim'];
+			$nama = $_REQUEST['nama'];
+			$email = $_REQUEST['email'];
+			$conn = mysqli_connect($host, $user, $inpt,$db);
+			if ($conn==false) {
+			die("Error BRAYY");				
+			}
+			$mysqli = "INSERT INTO modul5 (nim, nama, email) 
+					  VALUES ('$nim', '$nama', '$email')";
+			$result = mysqli_query($conn, $mysqli);
+			
+			echo "Registrasi Berhasil";  			
+
+		}
 
 
-?>
+ ?>
